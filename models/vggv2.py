e94a029a0fb21d205ed6685a85d8a37e2455882d
tcmyxc <1282494272@qq.com>
2022-01-13 08:12:37 +0000

    添加vggV2，可以自适应32x32以上的图片

---
+++ models/vggv2.py
@@ -0,0 +1,92 @@
+import torch
+import torch.nn as nn
+
+cfg = {
+    'A': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'B': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'D': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'E': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M']
+}
+
+
+class VGG(nn.Module):
+
+    def __init__(self, features, num_classes=10):
+        super().__init__()
+        self.features = features
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 49, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, num_classes)
+        )
+
+    def forward(self, x):
+        output = self.features(x)
+        output = self.avgpool(output)
+        output = torch.flatten(output, 1)
+        output = self.classifier(output)
+
+        return output
+
+
+def make_layers(cfg, in_channels=3, batch_norm=False):
+    layers = []
+
+    input_channel = in_channels
+    for l in cfg:
+        if l == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            continue
+
+        layers += [nn.Conv2d(input_channel, l, kernel_size=3, padding=1)]
+
+        if batch_norm:
+            layers += [nn.BatchNorm2d(l)]
+
+        layers += [nn.ReLU(inplace=True)]
+        input_channel = l
+
+    return nn.Sequential(*layers)
+
+
+def vgg11_bn(num_classes=100):
+    r"""VGG 11-layer model (configuration "A") with batch normalization
+    `"Very Deep Convolutional Networks For Large-Scale Image Recognition" <https://arxiv.org/pdf/1409.1556.pdf>`_.
+    The required minimum input size of the model is 32x32.
+    """
+    return VGG(make_layers(cfg['A'], batch_norm=True))
+
+
+def vgg13_bn():
+    r"""VGG 13-layer model (configuration "B")
+    `"Very Deep Convolutional Networks For Large-Scale Image Recognition" <https://arxiv.org/pdf/1409.1556.pdf>`_.
+    The required minimum input size of the model is 32x32.
+    """
+    return VGG(make_layers(cfg['B'], batch_norm=True))
+
+
+def vgg16_bn(in_channels=3, num_classes=10):
+    r"""VGG 16-layer model (configuration "D") with batch normalization
+    `"Very Deep Convolutional Networks For Large-Scale Image Recognition" <https://arxiv.org/pdf/1409.1556.pdf>`_.
+    The required minimum input size of the model is 32x32.
+    """
+    return VGG(make_layers(cfg['D'], batch_norm=True, in_channels=in_channels), num_classes=num_classes)
+
+
+def vgg19_bn():
+    r"""VGG 19-layer model (configuration "E")
+    `"Very Deep Convolutional Networks For Large-Scale Image Recognition" <https://arxiv.org/pdf/1409.1556.pdf>`_.
+    The required minimum input size of the model is 32x32.
+    """
+    return VGG(make_layers(cfg['E'], batch_norm=True))
+
+if __name__ == '__main__':
+    from torchsummary import summary
+    model = vgg16_bn().cuda()
+    # print(model)
+    summary(model=model, input_size=(3, 32, 32))
