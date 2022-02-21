a0e143606d004516b2e23b4535510f780bef7e36
tcmyxc <1282494272@qq,com>
2022-02-21 20:47:48 +0800

    余弦退火调整最后一个类别的卷积核

---
+++ modify_kernel/train.py
@@ -0,0 +1,177 @@
+import sys
+sys.path.append('/nfs/xwx/model-doctor-xwx')
+
+from loss.fl import focal_loss
+from loss.efl import equalized_focal_loss
+from loss.refl import reduce_equalized_focal_loss
+from loss.rfl import reduced_focal_loss
+from loss.dfl import dual_focal_loss
+
+import torch
+import torch.nn as nn
+from torch import optim
+import models
+import loaders
+
+from utils.lr_util import get_lr_scheduler
+from sklearn.metrics import classification_report
+
+from tqdm import tqdm
+import os
+
+# 梯度值
+modify_dict = {
+    -1: [64, [4, 19, 20, 27, 28, 36, 38, 40, 46, 50, 53]],
+}
+
+threshold = 0.25
+best_acc = 0
+
+def main():
+    # cfg
+    data_name = 'cifar-10-lt-ir100'
+    model_name = 'resnet32'
+    lr = 0.01
+    momentum = 0.9
+    weight_decay = 5e-4
+    epochs = 200
+    model_layers = [-1]
+
+    # device
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    print('-' * 79, '\n[Info] train on ', device)
+
+    # data
+    data_loaders, _ = loaders.load_data(data_name=data_name)
+
+    # model
+    model = models.load_model(
+        model_name=model_name,
+        in_channels=3,
+        num_classes=10
+    )
+
+    modules = models.load_modules(
+        model=model,
+        model_name=model_name,
+        model_layers=model_layers
+    )
+    # for layer in model.modules():
+    #     if(isinstance(layer, nn.Conv2d)):
+    #         print(layer.keys())
+    #         break
+    # print(modules)
+    # return
+    cp_path = os.path.join('/nfs/xwx/model-doctor-xwx/output/model/pretrained/resnet32-cifar-10-lt-ir100-refl-th-0.4-wr/checkpoint.pth')
+    if not os.path.exists(cp_path):
+        print("=" * 40)
+        print("模型文件的路径不存在, 请检查")
+        return
+    state = torch.load(cp_path)
+   
+    model.load_state_dict(state['model'])
+    model.to(device)
+
+
+    # cfg
+    loss_fn = reduce_equalized_focal_loss
+    optimizer = optim.SGD(
+        params=model.parameters(),
+        lr=lr,
+        momentum=momentum,
+        weight_decay=weight_decay
+    )
+    # scheduler = get_lr_scheduler(optimizer, True)
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(
+        optimizer=optimizer,
+        T_max=epochs
+    )
+
+    for t in range(epochs):
+        cur_lr = float(optimizer.state_dict()['param_groups'][0]['lr'])
+        print(f"\nEpoch {t+1}")
+        print("[INFO] lr is:", cur_lr)
+        print("-"*40)
+        train(data_loaders["train"], model, loss_fn, optimizer, modules, device)
+        test(data_loaders["val"], model, loss_fn, device)
+        scheduler.step()
+    print("Done!")
+
+    torch.save(model.state_dict(), "model-cos.pth")
+    print("Saved PyTorch Model State to model.pth")
+
+    # model.eval()
+    # test(data_loaders["val"], model, loss_fn, device)
+
+
+def train(dataloader, model, loss_fn, optimizer, modules, device):
+    # 这里加入了 classification_report
+    y_pred_list = []
+    y_train_list = []
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        y_train_list.extend(y.numpy())
+
+        X, y = X.to(device), y.to(device)
+
+        with torch.set_grad_enabled(True):
+            # Compute prediction error
+            pred = model(X)
+            loss = loss_fn(pred, y, threshold=threshold)
+        
+            y_pred_list.extend(pred.argmax(1).cpu().numpy())
+            
+            # Backpropagation
+            optimizer.zero_grad()
+            loss.backward()
+
+            for layer in modify_dict.keys():
+                for kernel_index in range(modify_dict[layer][0]):
+                    if kernel_index not in modify_dict[layer][1]:
+                        modules[int(layer)].weight.grad[kernel_index, ::] = 0
+            
+
+            optimizer.step()
+
+        if batch % 10 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+    
+    print("-" * 40)
+    print(classification_report(y_train_list, y_pred_list, digits=4))
+
+
+def test(dataloader, model, loss_fn, device):
+    global best_acc
+    # 这里加入了 classification_report
+    y_pred_list = []
+    y_train_list = []
+    
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    for X, y in dataloader:
+        y_train_list.extend(y.numpy())
+
+        X, y = X.to(device), y.to(device)
+        with torch.set_grad_enabled(True):
+            pred = model(X)
+            test_loss += loss_fn(pred, y, threshold=threshold).item()
+
+        y_pred_list.extend(pred.argmax(1).cpu().numpy())
+
+        correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+    test_loss /= num_batches
+    correct /= size
+    if correct > best_acc:
+        best_acc = correct
+        print("update best acc:", best_acc)
+    print(f"Test Error: Accuracy: {(100*correct):>0.2f}%, Avg loss: {test_loss:>8f} \n")
+    print(classification_report(y_train_list, y_pred_list, digits=4))
+
+
+if __name__ == '__main__':
+    main()
