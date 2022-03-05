c78463ea65dd103d5d56f7143b528bfe0c0f4ee5
tcmyxc <1282494272@qq,com>
2022-03-05 14:39:25 +0800

    添加类平衡采样数据加载器

--- loaders/__init__.py
+++ loaders/__init__.py
@@ -11,8 +11,8 @@ from loaders.mnin_loader import load_images_masks as load_mnin_images_masks
 # for long-tailed dataset
 from loaders.imagenet_lt_loader import load_images as load_imagenet_lt_images
 
-# from loaders.cifar_lt_loader import load_cifar_lt_images
-from loaders.cifar_lt_loader import load_class_balanced_cifar_lt_images as load_cifar_lt_images
+from loaders.cifar_lt_loader import load_cifar_lt_images
+from loaders.cifar_lt_loader import load_class_balanced_cifar_lt_images
 
 from loaders.inaturalist_data_loaders import load_images as load_inaturalist2018_images
 
@@ -124,3 +124,46 @@ def load_data(data_name, data_type=None, with_mask=False):
                 return load_mnin_images_masks(data_type)
             else:
                 return load_mnin_images(data_type)
+
+
+# 通过类平衡采样加载数据
+def load_class_balanced_data(data_name, data_type=None):
+    print('Load class balanced data:', data_name)
+
+    if data_type is None:
+        train_loader, test_loader, train_size, test_size = None, None, None, None
+        if data_name == 'cifar-10-lt-ir10':
+            train_loader, train_size = load_class_balanced_cifar_lt_images("train", data_name)
+            test_loader, test_size = load_class_balanced_cifar_lt_images("test", data_name)
+        elif data_name == 'cifar-10-lt-ir50':
+            train_loader, train_size = load_class_balanced_cifar_lt_images("train", data_name)
+            test_loader, test_size = load_class_balanced_cifar_lt_images("test", data_name)
+        elif data_name == 'cifar-10-lt-ir100':
+            train_loader, train_size = load_class_balanced_cifar_lt_images("train", data_name)
+            test_loader, test_size = load_class_balanced_cifar_lt_images("test", data_name)
+        elif data_name == 'cifar-100-lt-ir10':
+            train_loader, train_size = load_class_balanced_cifar_lt_images("train", data_name)
+            test_loader, test_size = load_class_balanced_cifar_lt_images("test", data_name)
+        elif data_name == 'cifar-100-lt-ir50':
+            train_loader, train_size = load_class_balanced_cifar_lt_images("train", data_name)
+            test_loader, test_size = load_class_balanced_cifar_lt_images("test", data_name)
+        elif data_name == 'cifar-100-lt-ir100':
+            train_loader, train_size = load_class_balanced_cifar_lt_images("train", data_name)
+            test_loader, test_size = load_class_balanced_cifar_lt_images("test", data_name)
+       
+        data_loaders = {'train': train_loader, 'val': test_loader}
+        dataset_sizes = {'train': train_size, 'val': test_size}
+        return data_loaders, dataset_sizes
+    else:
+        if data_name == 'cifar-10-lt-ir10':
+            return load_class_balanced_cifar_lt_images(data_type, data_name)
+        elif data_name == 'cifar-10-lt-ir50':
+            return load_class_balanced_cifar_lt_images(data_type, data_name)
+        elif data_name == 'cifar-10-lt-ir100':
+            return load_class_balanced_cifar_lt_images(data_type, data_name)
+        elif data_name == 'cifar-100-lt-ir10':
+            return load_class_balanced_cifar_lt_images(data_type, data_name)
+        elif data_name == 'cifar-100-lt-ir50':
+            return load_class_balanced_cifar_lt_images(data_type, data_name)
+        elif data_name == 'cifar-100-lt-ir100':
+            return load_class_balanced_cifar_lt_images(data_type, data_name)
