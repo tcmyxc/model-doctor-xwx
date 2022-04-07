7d96b2057751a1d93346024d4be5005c8a4e41de
tcmyxc <1282494272@qq.com>
2022-04-07 15:16:34 +0800

    把中间特征图作为模型第二个输出

--- models/resnetv2.py
+++ models/resnetv2.py
@@ -111,10 +111,11 @@ class ResNet(nn.Module):
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
+        feature_out = out
         out = F.avg_pool2d(out, out.size()[3])
         out = out.view(out.size(0), -1)
         out = self.linear(out)
-        return out
+        return out, feature_out
 
 
 def resnet20():
