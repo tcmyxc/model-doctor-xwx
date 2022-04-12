83923fa575dd7e9d862ef1dc4c9b70af60261c29
tcmyxc <1282494272@qq.com>
2022-04-12 14:19:49 +0800

    捕获CNN特征图作为第二个返回参数

--- models/resnetv2.py
+++ models/resnetv2.py
@@ -115,6 +115,7 @@ class ResNet(nn.Module):
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
+        feature = out
         out = F.avg_pool2d(out, out.size()[3])
 
         # xn = self.gru(torch.flatten(out, 2))
@@ -123,7 +124,7 @@ class ResNet(nn.Module):
 
         out = out.view(out.size(0), -1)
         out = self.linear(out)
-        return out
+        return out, feature
 
 
 def resnet20():
