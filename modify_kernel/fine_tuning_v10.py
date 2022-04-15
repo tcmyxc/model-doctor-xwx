d5a7939ffcd351dc81209122345d8000a0a53b75
tcmyxc <1282494272@qq.com>
2022-04-15 19:23:53 +0800

    优化结果路径

--- modify_kernel/fine_tuning_v10.py
+++ modify_kernel/fine_tuning_v10.py
@@ -89,6 +89,7 @@ def main():
     result_path = os.path.join(config.output_model, "three-stage",
                                model_name, data_name, 
                                f"lr{args.lr}", f"th{args.threshold}",
+                               args.lr_scheduler, args.loss_type,
                                get_current_time())
     if not os.path.exists(result_path):
         os.makedirs(result_path)
