c5eb3b3a43f278a7d59c7d5d79d887701cd5f022
tcmyxc <1282494272@qq.com>
2022-04-19 15:54:29 +0800

    优化自定义学习率调度器

--- utils/lr_util.py
+++ utils/lr_util.py
@@ -1,52 +1,32 @@
 from torch.optim import lr_scheduler
 
 
-# 前五轮预热，然后在160轮，180轮递减0.01
-def adjust_learning_rate(epoch):
+
+def _adjust_learning_rate(epoch, gamma=0.01):
     """Sets the learning rate"""
     if epoch < 5:
         decay = (epoch + 1) / 5
     elif epoch < 160:
         decay = 1
     elif epoch < 180:
-        decay = 0.01
+        decay = gamma
     else:
-        decay = 0.01**2
+        decay = gamma**2
 
     return decay
 
 
 # 前五轮预热，然后在160轮，180轮递减0.01
-def adjust_learning_rate_100x(epoch):
-    """Sets the learning rate"""
-    if epoch < 5:
-        decay = epoch / 4
-    elif epoch < 160:
-        decay = 1
-    elif epoch < 180:
-        decay = 0.01
-    else:
-        decay = 0.01**2
-
-    return decay
+def adjust_learning_rate(epoch):
+    return _adjust_learning_rate(epoch, 0.01)
 
 
 # 前五轮预热，然后在160轮，180轮递减0.1
 def adjust_learning_rate_10x(epoch):
     """Sets the learning rate"""
-    if epoch < 5:
-        decay = epoch / 4
-    elif epoch < 160:
-        decay = 1
-    elif epoch < 180:
-        decay = 0.1
-    else:
-        decay = 0.1**2
-
-    return decay
+    return _adjust_learning_rate(epoch, 0.1)
 
 
-# 前五轮预热，然后在160轮，180轮递减0.01
 def get_lr_scheduler(optimizer, verbose=False):
     scheduler = lr_scheduler.LambdaLR(
         optimizer,
@@ -57,22 +37,10 @@ def get_lr_scheduler(optimizer, verbose=False):
     return scheduler
 
 
-# 前五轮预热，然后在160轮，180轮递减0.1
 def get_lr_scheduler_10x(optimizer, verbose=False):
     scheduler = lr_scheduler.LambdaLR(
         optimizer,
         lr_lambda=adjust_learning_rate_10x,
-        verbose=verbose
-    )
-
-    return scheduler
-
-
-# 前五轮预热，然后在160轮，180轮递减0.01
-def get_lr_scheduler_100x(optimizer, verbose=False):
-    scheduler = lr_scheduler.LambdaLR(
-        optimizer,
-        lr_lambda=adjust_learning_rate_100x,
         verbose=verbose
     )
 
