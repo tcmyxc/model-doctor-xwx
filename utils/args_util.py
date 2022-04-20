f7c8d12b1473768a1fdc460573433bcfea1f938e
tcmyxc <1282494272@qq.com>
2022-04-20 12:24:16 +0800

    命令行参数打印工具类

---
+++ utils/args_util.py
@@ -0,0 +1,7 @@
+def print_args(args):
+    """优雅地打印命令行参数"""
+    
+    print("-" * 20, "args", "-" * 20)
+    for k, v in vars(args).items():
+        print(f"{k}: {v}")
+    print("-" * 18, "args end", "-" * 18)
