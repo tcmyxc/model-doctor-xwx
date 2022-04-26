b2e82ef005f3afd53af465863878b59f554776d2
tcmyxc <1282494272@qq.com>
2022-04-27 00:27:47 +0800

    纯分类训练模型及工具类

--- modify_kernel/util/draw_util.py
+++ modify_kernel/util/draw_util.py
@@ -5,9 +5,14 @@ matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import numpy as np
 
+from sklearn.metrics import classification_report
 
-def draw_acc_and_loss(result_path, train_loss, test_loss, train_acc, test_acc, filename=None):
+
+def draw_acc_and_loss(train_loss, test_loss, 
+                      train_acc, test_acc, 
+                      result_path, filename=None):
     """绘制acc和loss曲线"""
+    
     history = {
         "train_loss": train_loss,
         "train_acc": train_acc,
@@ -18,7 +23,7 @@ def draw_acc_and_loss(result_path, train_loss, test_loss, train_acc, test_acc, f
     if not os.path.exists(result_path):
         os.makedirs(result_path)
 
-    np.save(os.path.join(result_path, "model.npy" if filename is None else f"{filename}.npy"), history)
+    np.save(os.path.join(result_path, "model_acc_loss.npy" if filename is None else f"{filename}.npy"), history)
 
     num_epochs = len(train_loss)
 
@@ -33,12 +38,14 @@ def draw_acc_and_loss(result_path, train_loss, test_loss, train_acc, test_acc, f
     plt.ylabel("Acc & Loss")
     plt.legend(loc="upper right")
     plt.grid(True)
-    plt.savefig(os.path.join(result_path, "model.jpg" if filename is None else f"{filename}.jpg"))
+    plt.savefig(os.path.join(result_path, "model_acc_loss.jpg" if filename is None else f"{filename}.jpg"))
     plt.clf()
     plt.close()
 
 
 def draw_lr(result_path, lr_list):
+    """绘制lr曲线"""
+
     num_epochs = len(lr_list)
 
     plt.plot(range(1, num_epochs + 1), lr_list, label='lr')
@@ -49,5 +56,29 @@ def draw_lr(result_path, lr_list):
     plt.legend(loc="upper right")
     plt.grid(True)
     plt.savefig(os.path.join(result_path, "model_lr.jpg"))
+    plt.clf()
+    plt.close()
+
+
+def draw_classification_report(mode_type, result_path, y_train_list, y_pred_list):
+    """绘制在训练集/测试集上面的 classification_report"""
+
+    reports = classification_report(y_train_list, y_pred_list, digits=4, output_dict=True)
+    np.save(os.path.join(result_path, f"{mode_type}_classification_report.npy"), reports)
+
+    labels = []
+    accs = []
+    samplers =[]
+    for x_i, y_i in reports.items():
+        if x_i == "accuracy": break
+        labels.append(x_i)
+        accs.append(y_i["recall"])
+        samplers.append(y_i["support"])
+
+    plt.plot(labels, accs)
+    plt.title("Acc of each class")
+    plt.xlabel("Classes")
+    plt.ylabel("Accuracy")
+    plt.savefig(os.path.join(result_path, f"{mode_type}_classification_report.jpg"))
     plt.clf()
     plt.close()
