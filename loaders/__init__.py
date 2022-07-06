01fded017ad8fd401906d887489b85a9812bc23b
tcmyxc <1282494272@qq.com>
2022-07-06 20:05:29 +0800

    删除冗余代码

--- loaders/__init__.py
+++ loaders/__init__.py
@@ -5,9 +5,6 @@ from loaders.cifar100_loader import load_images as load_cifar100_images
 from loaders.imagenet_lt_loader import load_images as load_imagenet_lt_images
 from loaders.imagenet_lt_loader import load_class_balanced_imagenet_lt_images
 
-from loaders.imagenet_10_lt_loader import load_images as load_imagenet_10_lt_images
-from loaders.imagenet_10_lt_loader import load_class_balanced_imagenet_10_lt_images
-
 from loaders.cifar_lt_loader import load_cifar_lt_images
 from loaders.cifar_lt_loader import load_class_balanced_cifar_lt_images
 
@@ -27,9 +24,6 @@ def load_data(data_name, data_type=None, with_mask=False):
         elif data_name == 'imagenet-lt':
             train_loader, train_size = load_imagenet_lt_images('train')
             test_loader, test_size = load_imagenet_lt_images('test')
-        elif data_name == 'imagenet-10-lt':
-            train_loader, train_size = load_imagenet_10_lt_images('train')
-            test_loader, test_size = load_imagenet_10_lt_images('test')
         elif data_name == "inaturalist2018":
             train_loader, train_size = load_inaturalist2018_images("train")
             test_loader, test_size = load_inaturalist2018_images("test")
@@ -63,8 +57,6 @@ def load_data(data_name, data_type=None, with_mask=False):
             return load_cifar10_images(data_type)
         elif data_name == 'imagenet-lt':
             return load_imagenet_lt_images(data_type)
-        elif data_name == 'imagenet-10-lt':
-            return load_imagenet_10_lt_images(data_type)
         elif data_name == "inaturalist2018":
             return load_inaturalist2018_images(data_type)
         elif data_name == 'cifar-10-lt-ir10':
@@ -111,9 +103,6 @@ def load_class_balanced_data(data_name, data_type=None):
         elif data_name == "imagenet-lt":
             train_loader, train_size = load_class_balanced_imagenet_lt_images("train")
             test_loader, test_size   = load_class_balanced_imagenet_lt_images("test")
-        elif data_name == "imagenet-10-lt":
-            train_loader, train_size = load_class_balanced_imagenet_10_lt_images("train")
-            test_loader, test_size   = load_class_balanced_imagenet_10_lt_images("test")
        
         data_loaders = {'train': train_loader, 'val': test_loader}
         dataset_sizes = {'train': train_size, 'val': test_size}
@@ -133,24 +122,4 @@ def load_class_balanced_data(data_name, data_type=None):
             return load_class_balanced_cifar_lt_images(data_type, data_name)
         elif data_name == "imagenet-lt":
             return load_class_balanced_imagenet_lt_images(data_type)
-        elif data_name == "imagenet-10-lt":
-            return load_class_balanced_imagenet_10_lt_images(data_type)
-
-
-def load_single_class_images(data_name, data_type=None):
-    """只加载一个类别"""
-    from loaders.imagenet_10_lt_loader import load_single_class_images
-
-    print('-' * 42, f'\n[INFO] load singel class data: {data_name} \n', '-' * 42)
-
-    if data_type is None:
-        train_loader, test_loader, train_size, test_size = None, None, None, None
-
-        train_loader, train_size = load_single_class_images('train')
-        test_loader, test_size = load_single_class_images('test')
-
-        data_loaders = {'train': train_loader, 'val': test_loader}
-        dataset_sizes = {'train': train_size, 'val': test_size}
-        return data_loaders, dataset_sizes
-    else:
-        return load_single_class_images(data_type)
+        
