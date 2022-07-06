c947dd8d648808c354f43127eb59850c9847b0ca
tcmyxc <1282494272@qq.com>
2022-07-06 21:36:49 +0800

    删除冗余脚本

--- scripts/image_sift.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 /nfs/xwx/model-doctor-xwx/core/image_sift.py \
-    --data_name cifar-10-lt-ir10 \
-    --model_path /nfs/xwx/model-doctor-xwx/output/model/pretrained/resnet32/cifar-10-lt-ir10/lr0.1/custom_lr_scheduler/ce_loss/2022-07-01_00-25-40/best-model-acc0.8749.pth
