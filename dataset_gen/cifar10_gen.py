db85117efc88ad6554019f0189a54ad2f5a35124
tcmyxc <1282494272@qq.com>
2022-07-15 17:55:39 +0800

    注释掉无关的数据增强

---
+++ dataset_gen/cifar10_gen.py
@@ -0,0 +1,34 @@
+"""
+生成cifar10数据集
+"""
+if __name__ == '__main__':
+    import os
+    from torchvision import datasets
+    
+    trainset = datasets.CIFAR10(root='/nfs/xwx/dataset', train=True, download=True)
+    print("train dataset size:", len(trainset))
+    trainloader = iter(trainset)
+    root_dir = f"/nfs/xwx/dataset/cifar10/images/train"
+    print(root_dir)
+    for i, (data, label) in enumerate(trainloader):
+        img_path = root_dir + "/" + str(label)
+        if not os.path.exists(img_path):
+            os.makedirs(img_path)
+        filename = img_path + '/' + str(i) + '.png'
+        # print(filename)
+        data.save(filename)
+        
+    
+    testset = datasets.CIFAR10(root='/nfs/xwx/dataset', train=False, download=True)
+    print("test dataset size:", len(testset))
+    testloader = iter(testset)
+    root_dir = f"/nfs/xwx/dataset/cifar10/images/test"
+    print(root_dir)
+    for i, (data, label) in enumerate(testloader):
+        img_path = root_dir + "/" + str(label)
+        if not os.path.exists(img_path):
+            os.makedirs(img_path)
+        filename = img_path + '/' + str(i) + '.png'
+        # print(filename)
+        data.save(filename)
+        
